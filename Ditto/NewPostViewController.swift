fb6182093b83fe66b6de21eef014b63c842c41f1
Gurneev <sgurneev@gmail.com>
2016-04-16 22:46:19 -0700

    Picker view updates

--- Ditto/NewPostViewController.swift
+++ Ditto/NewPostViewController.swift
@@ -7,34 +7,64 @@
 //
 
 import UIKit
+//
+//protocol NewPostViewController: class {
+//
+//    dismissNewPostViewController(viewController: NewPostViewController)
+//    
+//
 
-protocol NewPostViewController: class {
-
-    dismissNewPostViewController(viewController: NewPostViewController)
+class NewPostViewControllerDelegate {
     
 }
 
-class NewPostViewController: UIViewController {
+class NewPostViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate{
 
     //Delegate Object
-    weak var delegate: NewPostViewControllerDelegate? {
-    }
+    weak var delegate: NewPostViewControllerDelegate?
+    
+    
+    
+    @IBOutlet weak var categoryField: UIPickerView!
+    
+    var pickerData: [String] = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "posted:", name: postNotification, object: nil)
+    
+    
+        self.categoryField.delegate = self
+        self.categoryField.dataSource = self
+        
+        // Input data into the Array:
+        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
+    
     }
 
     
     
-    func dismissNewPostViewController(viewContrller: NewPostViewController)
+    //func dismissNewPostViewController(viewContrller: NewPostViewController)
     
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
     
     func posted(notification: NSNotification){
         if let success = notification.object as? Bool {
@@ -58,6 +88,17 @@ class NewPostViewController: UIViewController {
     }
     
     
+    @IBOutlet weak var titleField: UITextField!
+    
+    @IBOutlet weak var descriptionField: UITextField!
+    
+    
+    
+    @IBAction func submit(sender: UIButton) {
+        var titleText = titleField.text
+        var descriptionText = descriptionField.text
+        var categoryChoice = categoryField.
+    }
     
     @IBOutlet weak var postTextView: UITextView!
 
